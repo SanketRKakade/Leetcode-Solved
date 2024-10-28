7e8c9dcdb2a4dfa7012c07de1560fb76cf20738c
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2024-10-28 21:33:33 +0530

    Time: 5 ms (90.46%), Space: 8.4 MB (79.55%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+
+private:
+    int expandFromCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+            
+            int len = max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        
+        return s.substr(start, maxLen);
+    }
+};
