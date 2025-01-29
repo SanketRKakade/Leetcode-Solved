d31c1fa51ded68bf90e1e08875be05ae6545e2e2
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-01-29 14:19:07 +0530

    Time: 2 ms (5.58%), Space: 7.4 MB (100%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-
-    int findAns(int n){
-        vector<int>dp(n+1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2 ; i<=n ; i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-    return dp[n];
-    }
     int climbStairs(int n) {
-        return findAns(n);
+        int prev2 = 1;
+        int prev = 1;
+        for(int i=2 ; i<=n ; i++){
+            int curr = prev + prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
-
 };
