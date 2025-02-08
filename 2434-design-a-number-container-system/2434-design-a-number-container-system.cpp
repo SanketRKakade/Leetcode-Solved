9078dd1087245c1fb8d81f16c2d0e7e2f793b164
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-02-08 12:38:39 +0530

    Time: 180 ms (5.63%), Space: 198.1 MB (6.88%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <set>
+using namespace std;
+
+class NumberContainers {
+    map<int, int> indexToNumber;
+    map<int, set<int>> numberToIndices;
+
+public:
+    NumberContainers() {}
+
+    void change(int index, int number) {
+        if (indexToNumber.count(index)) {
+            int oldNumber = indexToNumber[index];
+            numberToIndices[oldNumber].erase(index);
+            if (numberToIndices[oldNumber].empty()) {
+                numberToIndices.erase(oldNumber);
+            }
+        }
+        indexToNumber[index] = number;
+        numberToIndices[number].insert(index);
+    }
+
+    int find(int number) {
+        if (numberToIndices.count(number) && !numberToIndices[number].empty()) {
+            return *numberToIndices[number].begin();
+        }
+        return -1;
+    }
+};
