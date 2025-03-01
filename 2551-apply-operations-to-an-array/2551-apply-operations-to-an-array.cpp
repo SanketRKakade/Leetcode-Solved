512275957b79503d1a00ce8d34bbcc216945774e
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-03-02 00:08:21 +0530

    Time: 0 ms (100%), Space: 13.1 MB (62.06%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    void shiftZeroes(vector<int>& nums) {
+        int nonZeroIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[nonZeroIndex]);
+                nonZeroIndex++;
+            }
+        }
+    }
+
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0 ; i<nums.size()-1 ; i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]*=2;
+                nums[i+1]*=0;
+            }
+        }
+        shiftZeroes(nums);
+        return nums;
+    }
+};
