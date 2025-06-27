59f02a7ef8f9180308060db5975bb535dcbc22d2
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-06-27 16:37:39 +0530

    Time: 255 ms (5.79%), Space: 108.2 MB (6.23%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
 
-    void createSubset(vector<int>& nums, int index, vector<vector<int>>& res, vector<int>& subset) {
-        if (index == nums.size()) {
-            res.push_back(subset);
-            return;
-        }
+    vector<vector<int>> fun(int idx, vector<int>&nums, vector<vector<int>>&ans, vector<int>&temp){
+        
+        if(idx>=nums.size()){
+            ans.push_back(temp);
+            return ans;
+        } 
 
-        subset.push_back(nums[index]);
-        createSubset(nums, index + 1, res, subset);
+        temp.push_back(nums[idx]);
+        // ans.push_back(temp);
+        fun(idx+1,nums,ans,temp);
+        temp.erase(temp.begin()+idx);
+        // ans.push_back(temp);
+        fun(idx+1,nums,ans,temp);
+        return ans;
 
-        subset.pop_back();
-        createSubset(nums, index + 1, res, subset);
-    }    
-
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> subset;
-
-        createSubset(nums, 0, res, subset);
-        return res;        
+        vector<vector<int>> ans;
+        vector<int>temp;
+        return fun(0,nums,ans,temp);
     }
 };
