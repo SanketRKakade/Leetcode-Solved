91557d20a96a477ccf9b6b888e9781ea11f93d28
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-06-28 19:55:33 +0530

    Time: 2 ms (62.92%), Space: 8.1 MB (69.53%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,36 +1,38 @@
 class Solution {
-
-    void solve(int col, vector<string>& board, vector<vector<string>>& result, int n,
-               vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal) {
-        if (col == n) {
-            result.push_back(board);
-            return;
-        }
-        for (int row = 0; row < n; row++) {
-            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
-                solve(col + 1, board, result, n, leftRow, lowerDiagonal, upperDiagonal);
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
-                board[row][col] = '.';
-            }
-        }
-    }
-
 public:
-    int totalNQueens(int n) {
-        vector<vector<string>> result;
-        vector<string> board(n, string(n, '.'));
-        vector<int> leftRow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
-        solve(0, board, result, n, leftRow, lowerDiagonal, upperDiagonal);
-        return result.size();
+    
+
+void countSolutions(int col, int n, vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal, int& count) {
+    if (col == n) {
+        count++;
+        return;
     }
+
+    for (int row = 0; row < n; row++) {
+        if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
+
+            leftRow[row] = 1;
+            lowerDiagonal[row + col] = 1;
+            upperDiagonal[n - 1 + col - row] = 1;
+
+            countSolutions(col + 1, n, leftRow, lowerDiagonal, upperDiagonal, count);
+
+            leftRow[row] = 0;
+            lowerDiagonal[row + col] = 0;
+            upperDiagonal[n - 1 + col - row] = 0;
+        }
+    }
+}
+
+    int totalNQueens(int n) {
+        vector<int> leftRow(n, 0);
+        vector<int> lowerDiagonal(2 * n - 1, 0);
+        vector<int> upperDiagonal(2 * n - 1, 0);
+        int count = 0;
+
+        countSolutions(0, n, leftRow, lowerDiagonal, upperDiagonal, count);
+        return count;
+    }
+
+
 };
-
-
-
-
