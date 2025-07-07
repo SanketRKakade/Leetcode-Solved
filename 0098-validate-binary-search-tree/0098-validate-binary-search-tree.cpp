f878263807cedfe78dd73ecb4627ccbbf0a32e83
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-07-07 20:29:04 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.48%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,37 +6,19 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
 
-    bool solve(TreeNode* root ,long long int lowerBound ,long long int upperBound){
-        //base case
-        if(root==NULL){
-            return true;
-        }
-
-        bool cond1 = (root->val > lowerBound);
-        bool cond2 = (root->val < upperBound);
-        bool leftAns = solve(root->left,lowerBound, root->val );
-        bool rightAns = solve(root->right, root->val , upperBound);
-
-        if(cond1 && cond2 && leftAns && rightAns){
-            return true;
-        }
-        else{
-            return false;
-        }
-
+    bool solve(TreeNode* root, long long mini, long long maxi){
+        if(root==NULL) return true;
+        if(root->val >= maxi || root->val <=mini ) return false;
+        return solve(root->left, mini, root->val) && solve(root->right, root->val, maxi);
     }
-    bool isValidBST(TreeNode* root) {
-        long long int lowerBound = 	-2147483657;
-        long long int upperBound = 	2147483657;
 
-        bool ans = solve (root,lowerBound,upperBound);
-        return ans;
+    bool isValidBST(TreeNode* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
     }
 };
