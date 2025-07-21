22d9471d901bfa4c484d5635cc6a151283e87e49
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-07-21 14:26:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (54.87%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,24 @@
 class Solution {
 private:
-    int robLinearTab(vector<int>& nums) {
+    int robLinear(vector<int>& nums) {
         int n = nums.size();
         if (n == 0) return 0;
         if (n == 1) return nums[0];
 
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = 0;
+        int prev = nums[0];
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i] + prev2;
+            int not_pick = prev;
+            int curr = max(pick, not_pick);
+
+            // update for next step
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n-1];
+        return prev;
     }
 
 public:
@@ -24,8 +29,8 @@ public:
         vector<int> temp1(nums.begin() + 1, nums.end());     // Exclude first house
         vector<int> temp2(nums.begin(), nums.end() - 1);     // Exclude last house
 
-        int ans1 = robLinearTab(temp1);
-        int ans2 = robLinearTab(temp2);
+        int ans1 = robLinear(temp1);
+        int ans2 = robLinear(temp2);
 
         return max(ans1, ans2);
     }
