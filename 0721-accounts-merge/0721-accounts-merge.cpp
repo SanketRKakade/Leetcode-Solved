916b11afcc5eb83fa05811ed3caedfa6cefeca56
Sanket Rajendra Kakade <155830764+SanketRKakade@users.noreply.github.com>
2025-07-23 22:48:59 +0530

    Time: 28 ms (76.09%), Space: 40.2 MB (64.52%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
-    unordered_map<string,vector<string>> graph;
-    unordered_map<string,int> vis;
     
-    void dfs(string s,vector<string> &v){
-        vis[s]=1;
-        v.push_back(s);
-        for(auto it:graph[s]){
-            if(vis[it]==1)continue;
-            dfs(it,v);
+    int parent[1000];
+    int Size[1000];
+    void make(int n){
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            Size[i]=1;
         }
     }
+    int find(int i){
+        if(i==parent[i])return i;
+        return parent[i]=find(parent[i]);
+    }
+    void Union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b)return;
+        if(Size[b]>Size[a])swap(a,b);
+        parent[b]=a;
+        Size[a]+=Size[b];
+    }
+
 
     vector<vector<string>> accountsMerge(vector<vector<string>>& ac) {
+        unordered_map<string,int> mpp;
+        make(ac.size());
         for(int i=0;i<ac.size();i++){
-            for(int j=1;j<ac[i].size()-1;j++){
-                graph[ac[i][j]].push_back(ac[i][j+1]);
-                graph[ac[i][j+1]].push_back(ac[i][j]);
-                vis[ac[i][j]]=0;
-                vis[ac[i][j+1]]=0;
+            for(int j=1;j<ac[i].size();j++){
+                if(mpp.find(ac[i][j])!=mpp.end()){
+                    Union(i,mpp[ac[i][j]]);
+                }
+                else{
+                    mpp[ac[i][j]]=i;
+                }
             }
         }
-        vector<vector<string>> ans;
-        for(int i=0;i<ac.size();i++){
-            if(vis[ac[i][1]]==0){
-                vector<string> v;
-                v.push_back(ac[i][0]);
-                dfs(ac[i][1],v);
-                sort(++v.begin(),v.end());
-                ans.push_back(v);
-            }
+        vector<vector<string>> ans(ac.size());
+        for(auto it:mpp){
+            ans[find(it.second)].push_back(it.first);
         }
-        return ans;
+        vector<vector<string>> ans1;
+        for(int i=0;i<ac.size();i++){
+            if(ans[i].size()==0)continue;
+            vector<string> v;
+            v.push_back(ac[i][0]);
+            for(auto it:ans[i])v.push_back(it);
+            sort(++v.begin(),v.end());
+            ans1.push_back(v);
+        }
+        
+        return ans1;
     }
 };
